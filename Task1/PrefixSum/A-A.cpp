e03ae95d24abb91c8734fe544f516e8720222625
Hussein Alarini <hussainalarini.4444@gmail.com>
2025-12-09 17:54:32 +0200

    Add Prefix Sum solution in A-A.cpp

---
+++ Task1/PrefixSum/A-A.cpp
@@ -0,0 +1,46 @@
+// بسم الله الرحمن الرحيم 
+// Hussein El-sayed Alarini
+#include <bits/stdc++.h>
+using namespace std;
+
+#define bye return 0
+#define sp    " "
+#define ln    "\n"
+#define vi vector<int>
+#define se set<int>
+#define us unordered_set<int>
+#define ll long long
+#define ump unordered_map<int,int>
+#define mp map<int,int>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define Flash ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+template<typename T>
+istream& operator>>(istream& is, vector<T>& vec){for (auto& i : vec) is >> i; return is;}
+template<typename T>
+ostream& operator<<(ostream& os, vector<T>& vec){for (auto& i : vec) os << i << ' '; return os;}
+
+int solve()
+{
+    int a, b;
+    cin >> a >> b;
+    vector<long long> arr(a + 1), pri(a + 1);
+    for (int i = 1; i <= a; i++)cin >> arr[i];
+    pri[0] = 0;for (int i = 1; i <= a; i++)pri[i] = pri[i - 1] + arr[i];
+    while (b--) {
+        int l, r;
+        cin >> l >> r;
+        cout << pri[r] - pri[l - 1] << ln;
+    }
+    return 0;
+}
+
+signed main() {
+    Flash
+    // freopen("file.in", "r", stdin);
+    // freopen("file.out", "w", stdout);
+    int testCases = 1; //cin >> testCases;
+    while(testCases--) solve();
+    bye ;
+}
