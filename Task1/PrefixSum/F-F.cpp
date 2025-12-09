f7bff77c5a4281b79f6b426371cb88f240064726
Hussein Alarini <hussainalarini.4444@gmail.com>
2025-12-09 17:57:29 +0200

    Add PrefixSum algorithm implementation in F-F.cpp

---
+++ Task1/PrefixSum/F-F.cpp
@@ -0,0 +1,58 @@
+// بسم الله الرحمن الرحيم 
+// Hussein El-sayed Alarini
+#include <bits/stdc++.h>
+using namespace std;
+
+#define bye return 0
+#define sp    " "
+#define ln    "\n"
+#define vi vector<int>
+#define vil vector<long long>
+#define se set<int>
+#define us unordered_set<int>
+#define ll long long
+#define ump unordered_map<int,int>
+#define mp map<int,int>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define Flash ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+template<typename T>
+istream& operator>>(istream& is, vector<T>& vec){for (auto& i : vec) is >> i; return is;}
+template<typename T>
+ostream& operator<<(ostream& os, vector<T>& vec){for (auto& i : vec) os << i << ' '; return os;}
+
+int solve() {
+    int n, k, q;
+    cin >> n >> k >> q;
+    const int max = 200000;
+    vil dif(max + 3, 0);
+    for(int i = 0; i < n; i++) {
+        int l, r;
+        cin >> l >> r;
+        dif[l] += 1;
+        dif[r + 1] -= 1;
+    }
+    vil count(max + 2, 0);
+    for(int i = 1; i <= max; i++)count[i] = count[i - 1] + dif[i];
+    vil t(max + 2, 0);
+    for(int i = 1; i <= max; i++)t[i] = (count[i] >= k ? 1 : 0);
+    vil pri(max + 2, 0);
+    for(int i = 1; i <= max; i++)pri[i] = pri[i - 1] + t[i];
+       
+    for(int i = 0; i < q; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << pri[b] - pri[a - 1] << ln;
+    }
+    return 0;
+}
+
+signed main() {
+    Flash
+    // freopen("file.in", "r", stdin);
+    // freopen("file.out", "w", stdout);
+    int testCases = 1; //cin >> testCases;
+    while(testCases--) solve();
+    bye ;
+}
